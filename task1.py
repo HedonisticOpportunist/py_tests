dca4c6f1c4f9a6bc3eca6143980e6be9d6df324e
HedonisticOpportunist <fujitahibaraki@gmail.com>
2022-01-12 17:56:09 +0000

    Update task1.py

--- task1.py
+++ task1.py
@@ -5,7 +5,6 @@ def test_that_inverted_string_is_returned_for_an_argument_with_more_than_two_cha
     assert inverted_str == "tac"
     assert True
 
-
 def test_that_same_string_is_returned_for_an_argument_with_one_char():
     string_with_one_character = invert("a")
     assert string_with_one_character == "a"
@@ -19,4 +18,19 @@ def test_that_empty_string_is_returned_when_argument_is_an_empty_string():
 def test_that_empty_str_is_returned_when_argument_is_null():
     null_arg = invert(None)
     assert null_arg == ""
+    assert True
+
+def test_that_empty_str_is_not_returned_when_argument_is_a_string():
+    string_argument = invert("string")
+    assert string_argument != ""
+    assert True
+
+def test_that_non_empty_string_is_not_returned_when_argument_is_None():
+    null_argument = invert(None)
+    null_argument != "string"
+    assert True
+
+def test_that_str_is_not_more_than_two_chars_if_argument_is_one_char():
+    one_char_argument = invert("a")
+    assert one_char_argument != "ac"
     assert True
